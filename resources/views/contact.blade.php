bd00498e3bf47496cc98610a272330995bba4642
Christinabraham <mca514@rajagiri.edu>
2021-03-17 13:09:14 +0530

    facutitl

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -32,36 +32,33 @@
 </nav>
 <body>
 <div class="container">
-<table class="table">
+<table class="table table-borderless">
 <div class="row">
 <div class="col col-12 col-sm-2 col-lg-2 col-xl-2 col-xxl-2">
-
+</div>
 <div class="col col-12 col-sm-8 col-lg-8 col-xl-8 col-xxl-8">
-<table class="table">
 <tr>
     <td>NAME</td>
     <td><input type="text" class="form-control"></td>
 </tr>
 <tr>
-    <td>ROLL NUMBER</td>
+    <td>massege</td>
     <td><input type="text" class="form-control"></td>
 </tr>
 <tr>
-    <td>ADMISSION NUMBER</td>
+    <td>phone</td>
     <td><input type="text" class="form-control"></td>
-</tr>
-<tr>
-    <td>COLLEGE</td>
-    <td><input type="text" class="form-control"></td>
-</tr>
-<tr>
+    <tr>
     <td></td>
-    <td><BUTTON class="BTN BTN-PRIMARY">SUBMIT</BUTTON></td>
+    <td><button class="btn btn-primary">submit</button></td>
 </tr>
-</table>
-</div></div><div class="row">
-<div class="col col-12 col-sm-2 col-lg-2 col-xl-2 col-xxl-2">
+</tr>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</div>
+</table>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
 </body>
 </html>
