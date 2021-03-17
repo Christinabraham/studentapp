8bed6ab607aebe0f0b22ac67ad2a0947a0df549b
Christinabraham <mca514@rajagiri.edu>
2021-03-17 11:55:19 +0530

    student

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<table class="table">
+<div class="row">
+<div class="col col-12 col-sm-2 col-lg-2 col-xl-2 col-xxl-2">
+
+<div class="col col-12 col-sm-8 col-lg-8 col-xl-8 col-xxl-8">
+<table class="table">
+<tr>
+    <td>NAME</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>ROLL NUMBER</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>ADMISSION NUMBER</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>COLLEGE</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td></td>
+    <td><BUTTON class="BTN BTN-PRIMARY">SUBMIT</BUTTON></td>
+</tr>
+</table>
+</div></div><div class="row">
+<div class="col col-12 col-sm-2 col-lg-2 col-xl-2 col-xxl-2">
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
